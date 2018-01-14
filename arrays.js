d1f94639caa832ed99039aade21e7d8eaf4bb987
David Chen <clfhhc@gmail.com>
2018-01-14 08:32:42 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,11 +1,16 @@
 var chocolateBars=["snickers","hundred grand","kitkat","skittles"]
 
 function addElementToBeginningOfArray(array,element) {
+<<<<<<< HEAD
   return [element,...array];
+=======
+  return [element,...array]
+>>>>>>> f0a60d3b4cbfdbc38b8d33f77446d3f1a09cc868
 }
 
 function destructivelyAddElementToBeginningOfArray(array,element) {
   array.unshift(element);
+<<<<<<< HEAD
   return array;
 }
 
@@ -15,10 +20,13 @@ function addElementToEndOfArray(array,element) {
 
 function destructivelyAddElementToEndOfArray(array,element) {
   array.push(element);
+=======
+>>>>>>> f0a60d3b4cbfdbc38b8d33f77446d3f1a09cc868
   return array
 }
 
 function accessElementInArray(array,index) {
+<<<<<<< HEAD
   return array[index];
 }
 
@@ -38,4 +46,7 @@ function destructivelyRemoveElementFromEndOfArray(array) {
 
 function removeElementFromEndOfArray(array) {
   return array.slice(0,array.length-1);
+=======
+  
+>>>>>>> f0a60d3b4cbfdbc38b8d33f77446d3f1a09cc868
 }
